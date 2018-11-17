e889c12d0c2ae67d7eafdd52c8f36484feca3bd0
Alier <alier.akramov@zensoft.io>
2018-11-17 15:17:25 +0600

    First commit

---
+++ app/src/main/java/megaslot/game/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package megaslot.game.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
